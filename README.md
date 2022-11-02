8842f0b22ac24f13555a4c85af7c84d7d82785a5
GriffinDesign <111717492+GriffinDesign@users.noreply.github.com>
2022-11-01 22:55:30 -0500

    Added final codeto the project repo, Sorry for my lack of commits :(

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+
+
 # Professional-README-Generator
-Node.JS command line application that generates well-structured professional README.md files based on user input.
+
+# Table of Content
+-[description](#description)
+-[installation](#installation)
+-[usage](#usage)
+-[licenses](#licenses)
+-[contribution](#contribution)
+-[test](#test)
+-[username](#username)
+-[profile](#profile)
+
+undefined
+## username:
+
+    A command-line prompt that allows users to create well-structured professional README.md files based on the users input. After each question regarding content for the file is answered, the application formats it perfectly in an organized fashion.
+## description:
+
+    The installation process requires the user to fork the project directly from the original developers github repository list.
+## installation:
+
+    This project will be used to streamline the process of creating detailed README.md files by developers to save time while still adhering to best-practices and requirements for development jobs within the industry.
+## usage:
+
+    No licenses are required
+## licenses:
+
+    The sole contributor for the project was Colin Griffin
+## contribution:
+
+    The test process for the project was to complete all prompted questions until completetion to see whether or not the application took the input it was given and processed it into a structured file.
+## test:
+
+    undefined
+## email:
+
+    undefined
+## profile:
--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = ["Project title?","",  ];
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const markdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {   type: "input",
+        message: "What is the name of the project?",
+        name: "title"
+    },
+    {   
+        message: "Please provide a table on content",
+        name: "table of content"
+    },
+    {   
+        message: "What is the name of the user?",
+        name: "userName"
+    },
+    {  
+        message: "Please provide a description of the project",
+        name: "description"
+    },
+    {   
+        message: "What is the installation process?",
+        name: "installation"
+    },
+    {   
+        message: "How will this project be used?",
+        name: "usage"
+    },
+    {   
+        message: "What licenses are required with this project?",
+        name: "licenses"
+    },
+    {   
+        message: "Who were the contributors to this project?",
+        name: "contribution"
+    },
+    {   
+        message: "What is the test process for this project?",
+        name: "test"
+    },
+    {   
+        message: "What is the user github email address?",
+        name: "GitHub user email"
+    },
+    {   
+        message: "Please provide a profile picture",
+        name: "GitHub profile picture"
+    }
+]
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        fs.writeFile('README.md', generateMarkdown({...response}), (err) =>
+        err ? console.error(err) : console.log("File Created!")
+        );
+    });
+};
 
 // Function call to initialize app
 init();
